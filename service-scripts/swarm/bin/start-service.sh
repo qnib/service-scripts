1f08bfa8038230e3511f4cd1578113d4f145f9f1
Christian Kniep <christian@qnib.org>
2016-10-09 16:03:54 +0200

    react on org.qnib.service.depend_on label

--- service-scripts/swarm/bin/start-service.sh
+++ service-scripts/swarm/bin/start-service.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+### Return codes
+# - 100 Label 'org.qnib.service.depend_on' defines a service to depend on, but service is not present
+# - 101 No compose file found
+# - 102 compose file does not end with yml|yaml
+
+
 if [ "$#" -ne 1 ]; then
     echo "Illegal number of parameters '$#', please only provide compose file"
     exit 1
@@ -12,7 +19,7 @@ SRV_MODE=${SRV_MODE:-replicated}
 COMPOSE_FILE=$(find ${1} -name "docker-compose*")
 if [ "X${COMPOSE_FILE}" == "X" ];then
   echo "!! Could not find compose file... exit"
-  exit 1
+  exit 101
 fi
 COMPOSE_JSON=$(echo ${COMPOSE_FILE} |sed -e 's/yaml/json/')
 if [ ${COMPOSE_FILE} == ${COMPOSE_JSON} ];then
@@ -20,7 +27,7 @@ if [ ${COMPOSE_FILE} == ${COMPOSE_JSON} ];then
 fi
 if [ ${COMPOSE_FILE} == ${COMPOSE_JSON} ];then
     echo "!! yaml and json file are equal... (${COMPOSE_FILE} == ${COMPOSE_JSON})"
-    exit 1
+    exit 102
 fi
 echo ">> COMPOSE_FILE=${COMPOSE_FILE}"
 python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' < ${COMPOSE_FILE} > ${COMPOSE_JSON}
@@ -38,7 +45,7 @@ for srv in $(egrep -o "[a-z0-9\-]+:\s+\#[a-z0-9]+" ${COMPOSE_FILE} |sed -e 's/ /
   fi
   echo ">> Looking into Service '${ADV_SRV_NAME}', expected scale '${SERVICE_SCALE}'"
   CUR_SRV_MODE=$(docker service inspect -f '{{json .Spec.Mode }}' ${ADV_SRV_NAME} |jq '. |keys[] |ascii_downcase' |tr -d '"')
-  CUR_SVC_IMG=$(docker service ls |grep ${ADV_SRV_NAME} |awk '{print $4}')
+  CUR_SVC_IMG=$(docker service ls -f name=${ADV_SRV_NAME} |grep -v "^ID" |awk '{print $4}')
   if [ "X${CUR_SVC_IMG}" != "X" ];then
     CUR_SVC_CNT=$(docker service ps --filter desired-state=running ${ADV_SRV_NAME} |grep -c ${ADV_SRV_NAME})
     echo ">>>>> Current service count '${CUR_SVC_CNT}'"
@@ -66,7 +73,7 @@ for srv in $(egrep -o "[a-z0-9\-]+:\s+\#[a-z0-9]+" ${COMPOSE_FILE} |sed -e 's/ /
       SVC_PORTS="${SVC_PORTS} --publish ${SVC_PORT}"
   done
   ## Volumes
-  SRV_MNT_CNT="$(jq ".services.\"${SERVICE_NAME}\".volumes[]" ${COMPOSE_JSON} |tr -d '"' |wc -l)"
+  SRV_MNT_CNT=$(jq ".services.\"${SERVICE_NAME}\".volumes[]" ${COMPOSE_JSON} |tr -d '"' |wc -l)
   if [ ${SRV_MNT_CNT} -ne 0 ];then
     SRV_MNT=""
     for SMNT in $(jq ".services.\"${SERVICE_NAME}\".volumes[]" ${COMPOSE_JSON} |tr -d '"' |xargs);do
@@ -84,7 +91,7 @@ for srv in $(egrep -o "[a-z0-9\-]+:\s+\#[a-z0-9]+" ${COMPOSE_FILE} |sed -e 's/ /
     done
   fi
   ## Environment
-  SRV_ENV_CNT="$(jq ".services.\"${SERVICE_NAME}\".environment[]" ${COMPOSE_JSON} |tr -d '"' |wc -l)"
+  SRV_ENV_CNT=$(jq ".services.\"${SERVICE_NAME}\".environment[]" ${COMPOSE_JSON} |tr -d '"' |wc -l)
   if [ ${SRV_ENV_CNT} -ne 0 ];then
     SRV_ENV=""
     for SENV in $(jq ".services.\"${SERVICE_NAME}\".environment[]" ${COMPOSE_JSON} |tr -d '"' |xargs);do
@@ -98,9 +105,34 @@ for srv in $(egrep -o "[a-z0-9\-]+:\s+\#[a-z0-9]+" ${COMPOSE_FILE} |sed -e 's/ /
   else
     SRV_ENV=""
   fi
-  if [ "X${EXTRA_SRV_ENV}" != "X" ];then
-    SRV_ENV=$(echo "${SRV_ENV} -e ${EXTRA_SRV_ENV}")
+  ## Labels
+  SRV_LABELS_CNT=$(jq ".services.\"${SERVICE_NAME}\".labels[]" ${COMPOSE_JSON} |tr -d '"' |wc -l)
+  if [ ${SRV_LABELS_CNT} -ne 0 ];then
+      SRV_LABELS=""
+      for SLAB in $(jq ".services.\"${SERVICE_NAME}\".labels[]" ${COMPOSE_JSON} |tr -d '"' |xargs);do
+          SLAB_KEY=$(echo ${SLAB} |awk -F\= '{print $1}')
+          SLAB_VAL=$(echo ${SLAB} |awk -F\= '{print $2}')
+          if [  ${SLAB_KEY} == "org.qnib.service.depend_on" ];then
+              # check if dependency is up'n'runnin
+              if [ $(docker service ls -q -f name=${SLAB_VAL} |wc -l) -eq 0 ];then
+                  echo "!! Service dependency '${SLAB_VAL}' is not running, exit..."
+                  docker service ls
+                  exit 100
+              else
+                  echo ">>> Service dependency '${SLAB_VAL}' is running, off we go.."
+              fi
+          else
+              SRV_LABELS="${SRV_LABELS} ${SLAB}"
+          fi
+      done
+      SRV_LABELS=$(echo ${SRV_LABELS} |sed -e 's/ / --label /g')
+      if [ "X${SRV_LABELS}" != "X" ];then
+            SRV_LABELS=$(echo ${SRV_LABELS} |sed -e 's/^/--label /')
+      fi
+  else
+    SRV_LABELS=""
   fi
+  # Replicatited of global
   if [ ${SRV_MODE} == "global" ];then
     SCALE_OPTS="--mode global"
   else
@@ -108,17 +140,20 @@ for srv in $(egrep -o "[a-z0-9\-]+:\s+\#[a-z0-9]+" ${COMPOSE_FILE} |sed -e 's/ /
   fi
   if [ ${CUR_SVC_CNT} -eq 0 ];then
       echo "No service running..."
+
       set -e
       echo ">>> docker service create --name ${ADV_SRV_NAME} ${SRV_ENV} \ "
       echo "                ${SCALE_OPTS} ${SVC_NETS} \ "
       echo "                ${SVC_PORTS} \ "
       echo "                ${SRV_MNT} \ "
+      echo "                ${SRV_LABELS} \ "
       echo "                ${LATEST_SVC_IMG}"
       set -e
       docker service create --name ${ADV_SRV_NAME} ${SRV_ENV} \
                             ${SCALE_OPTS} ${SVC_NETS} \
                             ${SVC_PORTS} \
                             ${SRV_MNT} \
+                            ${SRV_LABELS} \
                             ${LATEST_SVC_IMG}
       set +e
   else
@@ -136,4 +171,4 @@ for srv in $(egrep -o "[a-z0-9\-]+:\s+\#[a-z0-9]+" ${COMPOSE_FILE} |sed -e 's/ /
 
 done
 
-rm -f ${COMPOSE_JSON}
+#rm -f ${COMPOSE_JSON}
