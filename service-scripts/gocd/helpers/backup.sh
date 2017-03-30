ca51a0284593e4096ef5ff70a4bd604f67f835d3
Christian Kniep <christian@qnib.org>
2017-03-30 15:05:35 +0200

    add logs to backup script

--- service-scripts/gocd/helpers/backup.sh
+++ service-scripts/gocd/helpers/backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+echo "[II] >> Trigger backup via 'http://${GO_SERVER:-localhost}:8153/go/api/backups'"
 curl -X POST \
      -H 'Confirm: true' -H 'Accept: application/vnd.go.cd.v1+json' \
      http://${GO_SERVER:-localhost}:8153/go/api/backups 
