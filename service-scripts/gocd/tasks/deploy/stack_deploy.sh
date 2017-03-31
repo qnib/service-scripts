f94139f39f89088db8905746e70fb3cebe842242
Christian Kniep <christian@qnib.org>
2017-03-31 17:27:27 +0200

    use service-specific predeploy.sh

--- service-scripts/gocd/tasks/deploy/stack_deploy.sh
+++ service-scripts/gocd/tasks/deploy/stack_deploy.sh
@@ -3,6 +3,27 @@
 PNAME=$(echo ${GO_PIPELINE_NAME} |sed -e "s/${GO_ENVIRONMENT_NAME}-//")
 
 cd ${PNAME}
-if [[ -x ./deploy.sh ]];then
-    ./deploy.sh
+if [[ -x ./predeploy.sh ]];then
+    ./predeploy.sh
 fi
+/opt/service-scripts/gocd/common/bin/download-artifacts.sh
+
+## Which current images are used?
+rm -f temp.env
+docker service ls |grep "${PNAME}_" |awk '{print $2" "$5}' | while read l
+do
+  SVC_NAME=$(echo $l |awk '{print $1}' |cut -d'_' -f 2- |tr '[:lower:]' '[:upper:]')
+  IMG_FULL=$(echo $l |awk '{print $2}')
+  IMG_NAME=$(echo ${IMG_FULL} |awk -F/ '{print $NF}' |awk -F'\:|@' '{print $1}')
+  echo "[DEBUG] ${SVC_NAME} uses currently ${IMG_FULL}"
+  if [[ -f ./target/${IMG_NAME}.image_name ]];then
+      IMG_FULL=$(cat ./target/${IMG_NAME}.image_name)
+      echo "[DEBUG] Found updated image by parent, now ${IMG_FULL}"
+  fi
+  echo "export ${SVC_NAME}_IMG=${IMG_FULL}" >> temp.env
+done
+source temp.env
+rm -f temp.env
+
+echo "[II] Deploy stack from docker-compose file"
+docker stack deploy --compose-file docker-compose.yml ${PNAME}
