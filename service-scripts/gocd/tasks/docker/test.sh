45601d81ad2904247df691a6406b54276674630c
Christian Kniep <christian@qnib.org>
2017-06-13 12:56:47 +0200

    allow for the test to sit in docker directory

--- service-scripts/gocd/tasks/docker/test.sh
+++ service-scripts/gocd/tasks/docker/test.sh
@@ -6,6 +6,10 @@ source /opt/service-scripts/gocd/helpers/gocd-functions.sh
 # Create BUILD_IMG_NAME, which includes the git-hash and the revision of the pipeline
 assemble_build_img_name
 
+if [[ -d docker ]];then
+    cd docker
+fi
+
 if [ "X${SKIP_TEST}" != "Xtrue" ] && [ -d test ];then
     echo ">>>> Run test"
     cd test
