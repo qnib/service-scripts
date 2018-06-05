d505dd92118c99ee358459a44b27189f6bb98add
Christian Kniep <christian@qnib.org>
2018-06-05 20:37:55 +0200

    allow build to run on local node

--- service-scripts/gocd/helpers/ucp.sh
+++ service-scripts/gocd/helpers/ucp.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 : ${HOME_DIR:="/home"}
 
+
 function ucp_source_bundle {
+  docker info 2>/dev/null |awk '/Name:/{print $2}' > ~/docker-node
   if [[ "X${HOME_DIR}" != "X" ]] && [[ -f "${HOME_DIR}/${UCP_USER}/bundle/env.sh" ]];then
     echo ">> Source bundle '${HOME_DIR}/${UCP_USER}/bundle/env.sh'"
     pushd ${HOME_DIR}/${UCP_USER}/bundle/ >/dev/null
