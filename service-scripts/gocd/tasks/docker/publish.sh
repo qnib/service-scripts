f190d06f053b2dafadf38385807bfe3f128bc695
Christian Kniep <christian@qnib.org>
2018-06-07 11:52:33 +0200

    allow for client login to DTR

--- service-scripts/gocd/tasks/docker/publish.sh
+++ service-scripts/gocd/tasks/docker/publish.sh
@@ -13,9 +13,12 @@ fi
 source /opt/service-scripts/gocd/helpers/gocd-functions.sh
 # Create BUILD_IMG_NAME, which includes the git-hash and the revision of the pipeline
 assemble_build_img_name
-source /opt/service-scripts/gocd/helpers/ucp.sh
-ucp_source_bundle
-
+if [[ "X${DOCKER_REG}" != "X" && "X${DOCKER_REGISTRY_PASSWD}" != "X" && "X${DOCKER_REGISTRY_USER}" != "X" ]];then
+  docker login -u ${DOCKER_REGISTRY_USER} -p ${DOCKER_REGISTRY_PASSWD} ${DOCKER_REG}
+else
+  source /opt/service-scripts/gocd/helpers/ucp.sh
+  ucp_source_bundle
+fi
 if [[ $(basename $(pwd)) == "docker" ]];then
     ARTIFACTS_DIR=".."
 else
@@ -29,10 +32,21 @@ if [ "${SKIP_TAG_LATEST}" == "false" ];then
       echo ">> Publish >>> Push image to ${DOCKER_REG}: docker tag/push/rmi ${DOCKER_REG}/${DOCKER_REPO}/${IMG_NAME}:${DOCKER_TAG}"
       docker tag ${BUILD_IMG_NAME} ${DOCKER_REG}/${DOCKER_REPO}/${IMG_NAME}:${DOCKER_TAG}
       set +e
+      begin=$(date +%s)
       docker push ${DOCKER_REG}/${DOCKER_REPO}/${IMG_NAME}:${DOCKER_TAG}
       if [[ $? -ne 0 ]];then
-        echo "[!!] Push failed "
-        exit 1
+        end=$(date +%s)
+        if [[ $(($end-$begin)) -lt 300 ]];then
+          echo "[!!] Push failed ($(($end-$begin))<300s)"
+          exit 1
+        else
+          echo "[WW] Push failed (>300s), try once more"
+          docker push ${DOCKER_REG}/${DOCKER_REPO}/${IMG_NAME}:${DOCKER_TAG}
+          if [[ $? -ne 0 ]];then
+            echo "[!!] Push failed second time (<500s)"
+            exit 1
+          fi
+        fi
       fi
       set -e
       BUILD_IMG_REPODIGEST=$(docker inspect -f '{{(index .RepoDigests 0) }}' ${DOCKER_REG}/${DOCKER_REPO}/${IMG_NAME}:${DOCKER_TAG} |awk -F\@ '{print $2}')
@@ -52,7 +66,24 @@ if [ "${DOCKER_TAG_REV}" == "true" ];then
     if [ "X${DOCKER_REG}" != "X" ];then
         echo ">> Publish >>> Tag image remotely with build revision: docker tag/push/rmi ${DOCKER_REG}/${BUILD_REV_NAME}"
         docker tag ${BUILD_IMG_NAME} ${DOCKER_REG}/${BUILD_REV_NAME}
+        set +e
+        begin=$(date +%s)
         docker push ${DOCKER_REG}/${BUILD_REV_NAME}
+        if [[ $? -ne 0 ]];then
+          end=$(date +%s)
+          if [[ $(($end-$begin)) -lt 200 ]];then
+            echo "[!!] Push failed (<200s)"
+            exit 1
+          else
+            echo "[WW] Push failed (>200s), try once more"
+            docker push ${DOCKER_REG}/${BUILD_REV_NAME}
+            if [[ $? -ne 0 ]];then
+              echo "[!!] Push failed second time. :("
+              exit 1
+            fi
+          fi
+        fi
+        set -e
         BUILD_IMG_REPODIGEST=$(docker inspect -f '{{(index .RepoDigests 0) }}' ${DOCKER_REG}/${BUILD_REV_NAME} |awk -F\@ '{print $2}')
         IMG_FULL_NAME="${DOCKER_REG}/${BUILD_REV_NAME}@${BUILD_IMG_REPODIGEST}"
         echo ">> Full name of image: ${IMG_FULL_NAME}"
