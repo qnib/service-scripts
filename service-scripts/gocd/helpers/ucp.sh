75ace0b0353e69887b0a374524db366358ed2be3
Christian Kniep <christian@qnib.org>
2018-12-31 11:16:42 +0100

    fix ucp.sh

--- service-scripts/gocd/helpers/ucp.sh
+++ service-scripts/gocd/helpers/ucp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 : ${HOME_DIR:="/home"}
 
 
@@ -16,8 +17,8 @@ function ucp_source_bundle {
 }
 
 function docker_login {
-  if [[ "X${DOCKER_REGISTRY}" != "X" ]] && [[ "X${DOCKER_USER}" != "X" ]] && [[ -f "/run/secrets/${DOCKER_REGISTRY}/${DOCKER_USER/password" ]];then
-      docker login ${DOCKER_REGISTRY} --username ${DOCKER_USER} --password $(cat "/run/secrets/${DOCKER_REGISTRY}/${DOCKER_USER/password")
+  if [[ "X${DOCKER_REGISTRY}" != "X" ]] && [[ "X${DOCKER_USER}" != "X" ]] && [[ -f "/run/secrets/${DOCKER_REGISTRY}/${DOCKER_USER}/password" ]];then
+      docker login ${DOCKER_REGISTRY} --username ${DOCKER_USER} --password $(cat "/run/secrets/${DOCKER_REGISTRY}/${DOCKER_USER}/password")
   else
       echo "[EE] Please provide DOCKER_REGISTRY, DOCKER_USER and mount the password/token to /run/secrets/DOCKER_REGISTRY/DOCKER_USER/password"
   fi
